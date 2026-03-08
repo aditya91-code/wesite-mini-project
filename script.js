16ecacce882903ac3a170c4b95e3493347566a3c
aditya91-code <devsyadav0101@gmail.com>
2026-03-08 21:22:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function validateForm(){
+
+var name=document.getElementById("name").value;
+var email=document.getElementById("email").value;
+
+if(name=="" || email==""){
+alert("Please fill all fields");
+return false;
+}
+
+alert("Form submitted successfully!");
+}
+
+function showOffer(){
+alert("Special Offer: 5% discount on sports car booking!");
+}
